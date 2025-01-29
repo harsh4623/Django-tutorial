8cba32ca0d56478e4e806bfe951a6ff8c3263a7e
harsh4623 <harshpatel46399@gmail.com>
2025-01-29 16:51:37 +0530

    add new files

---
+++ pr6_E_comm/shop/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+def index(request):
+    product=Product.objects.all()
+    print(product)
+    n=len(product)
+    nSlides=n//4 + ceil((n/4)- (n//4))
+    allProd=[[product,range(1,len(product)),nSlides],[product,range(1,len(product)),nSlides]]
+    params={'allProd':allProd}
+    return render(request,'shop/index.html',params)
+
+def about(request):
+    return render(request,'shop/about.html')
+
+def contact(request):
+    return HttpResponse("We are at contact")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
