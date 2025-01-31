2924b9d83cfece105b23aa013566ce062ca800fc
harsh4623 <harshpatel46399@gmail.com>
2025-01-31 15:40:45 +0530

    new

--- pr6_E_comm/shop/views.py
+++ pr6_E_comm/shop/views.py
@@ -1,31 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 from math import ceil
 
 def index(request):
     product=Product.objects.all()
-    print(product)
-    n=len(product)
-    nSlides=n//4 + ceil((n/4)- (n//4))
-    allProd=[[product,range(1,len(product)),nSlides],[product,range(1,len(product)),nSlides]]
-    params={'allProd':allProd}
+
+    allProds=[]
+
+    catProd=Product.objects.values('category','id')
+    cats={item['category'] for item in catProd}
+
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n=len(prod) 
+        nSlides=n//4 +ceil((n/4)-(n//4))
+        allProds.append([prod,range(1,nSlides),nSlides])
+
+    params={'allProd':allProds}
     return render(request,'shop/index.html',params)
 
 def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    
+    if request.method=="POST":
+        print(request)
+        name=request.POST.get('name','')
+        email=request.POST.get('email','')
+        phone=request.POST.get('phone','')
+        desc=request.POST.get('desc','')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
+    return render(request,'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request,'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request,'shop/search.html')
 
-def productView(request):
-    return HttpResponse("We are at product view")
+def productView(request,myid):
+    product=Product.objects.filter(id=myid)
+    print(product)
+    return render(request,'shop/prodView.html',{'product':product[0]})
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request,'shop/checkout.html')
